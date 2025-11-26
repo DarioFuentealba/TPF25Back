01170cce0ba282309dd3c0a49a8adca872935ba6
DarioFuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2025-11-26 16:31:58 -0300

    prueba 3

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# apuntar al settings.py dentro de backend
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
